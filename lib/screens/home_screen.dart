32528282fcf80d4b304dba501f7408c586feaa95
Eren Toksöz <eren.toksoz@hotmail.com>
2025-01-22 20:54:56 +0300

    SQLflite database added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'pet_list_screen.dart';
-import 'settings_screen.dart';
+import 'package:pet_tracking_app/services/database_service.dart';
+
+import '../models/task.dart';
+// import 'pet_list_screen.dart';
+// import 'settings_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,52 +13,137 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-  final PageController _pageController = PageController();
+  final DatabaseService _databaseService = DatabaseService.instance;
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    _pageController.jumpToPage(index);
-  }
+  String? _task = null;
+
+  // int _selectedIndex = 0;
+  // final PageController _pageController = PageController();
+
+  // void _onItemTapped(int index) {
+  //   setState(() {
+  //     _selectedIndex = index;
+  //   });
+  //   _pageController.jumpToPage(index);
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: _addTaskButton(),
+      body: _tasksList(),
       appBar: AppBar(title: const Text('Ana Sayfa')),
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        children: const [
-          PetListScreen(),
-          Center(child: Text('Evcil Hayvan Takip Uygulamasına Hoşgeldiniz!')),
-          SettingsScreen(),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'Evcil Hayvanlar',
+      // body: PageView(
+      //   controller: _pageController,
+      //   onPageChanged: (index) {
+      //     setState(() {
+      //       _selectedIndex = index;
+      //     });
+      //   },
+      //   children: const [
+      //     PetListScreen(),
+      //     Center(child: Text('Evcil Hayvan Takip Uygulamasına Hoşgeldiniz!')),
+      //     SettingsScreen(),
+      //   ],
+      // ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.list),
+      //       label: 'Evcil Hayvanlar',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: 'Ana Sayfa',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.settings),
+      //       label: 'Ayarlar',
+      //     ),
+      //   ],
+      //   currentIndex: _selectedIndex,
+      //   selectedItemColor: Colors.blue,
+      //   onTap: _onItemTapped,
+      // ),
+    );
+  }
+
+  Widget _addTaskButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text("Add Task"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      _task = value;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Evcil Hayvan Adı',
+                  ),
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  onPressed: () {
+                    if (_task == null || _task == "") return;
+                    _databaseService.addTask(_task!);
+                    setState(() {
+                      _task = null;
+                    });
+                    Navigator.pop(
+                      context,
+                    );
+                  },
+                  child: const Text(
+                    'Ekle',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Ana Sayfa',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Ayarlar',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        onTap: _onItemTapped,
-      ),
+        );
+        // _databaseService.addTask('Yeni Görev');
+      },
+      child: const Icon(Icons.add),
+    );
+  }
+
+  Widget _tasksList() {
+    return FutureBuilder(
+      future: _databaseService.getTask(),
+      builder: (context, snapshot) {
+        return ListView.builder(
+          itemCount: snapshot.data?.length ?? 0,
+          itemBuilder: (context, index) {
+            Task task = snapshot.data![index];
+            return ListTile(
+              onLongPress: () {
+                _databaseService.deleteTask(
+                  task.id,
+                );
+                setState(() {});
+              },
+              title: Text(
+                task.content,
+              ),
+              // trailing: Checkbox(
+              //   value: false,
+              //   onChanged: (value) {},
+              // ),
+            );
+          },
+        );
+      },
     );
   }
 }
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,3 +1,98 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import '../models/task.dart';
+
 class DatabaseService {
-  static final DatabaseService instance = DatabaseService.instance;
+  static Database? _db;
+  static final DatabaseService instance = DatabaseService._constructor();
+
+  final String _tasksTableName = "tasks";
+  final String _tasksIdColumnName = "id";
+  final String _tasksContentColumnName = "content";
+  final String _tasksStatusColumnName = "status";
+
+  DatabaseService._constructor();
+
+  Future<Database> get database async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await getDatabase();
+    return _db!;
+  }
+
+  Future<Database> getDatabase() async {
+    final databasedirPath = await getDatabasesPath();
+    final databasePath = join(databasedirPath, "master_db.db");
+    final database = await openDatabase(
+      databasePath,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute(''' CREATE TABLE $_tasksTableName(
+          $_tasksIdColumnName INTEGER PRIMARY KEY,
+          $_tasksContentColumnName TEXT NOT NULL,
+          $_tasksStatusColumnName INTEGER NOT NULL
+        )''');
+      },
+    );
+    return database;
+  }
+
+  void addTask(
+    String content,
+  ) async {
+    final db = await database;
+    await db.insert(_tasksTableName, {
+      _tasksContentColumnName: content,
+      _tasksStatusColumnName: 0,
+    });
+  }
+
+  void deleteTask(int id) async {
+    final db = await database;
+    await db.delete(
+      _tasksTableName,
+      where: 'id = ?',
+      whereArgs: [
+        id,
+      ],
+    );
+  }
+
+  void updateTaskStatus(int id, int status) async {
+    final db = await database;
+    await db.update(
+      _tasksTableName,
+      {
+        _tasksStatusColumnName: status,
+      },
+      where: 'id = ?',
+      whereArgs: [
+        id,
+      ],
+    );
+  }
+
+  Future<List<Task>> getTask() async {
+    final db = await database;
+    final data = await db.query(_tasksTableName);
+    List<Task> tasks = data
+        .map((e) => Task(
+            id: e["id"] as int,
+            status: e["status"] as int,
+            content: e["content"] as String))
+        .toList();
+    return tasks;
+  }
 }
+
+// onCreate: (db, version) {
+      //   db.execute('''
+      //     CREATE TABLE pets(
+      //       id INTEGER PRIMARY KEY AUTOINCREMENT,
+      //       name TEXT,
+      //       species TEXT,
+      //       breed TEXT,
+      //       birthDate TEXT,''');
+      // },
