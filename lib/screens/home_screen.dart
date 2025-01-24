c93ddfe7bf5982cfa0c7f0ba7f5a5f1d35105a8c
Eren Toksöz <eren.toksoz@hotmail.com>
2025-01-24 17:27:39 +0300

    deleted unreq pages

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,54 +17,12 @@ class _HomeScreenState extends State<HomeScreen> {
 
   String? _task = null;
 
-  // int _selectedIndex = 0;
-  // final PageController _pageController = PageController();
-
-  // void _onItemTapped(int index) {
-  //   setState(() {
-  //     _selectedIndex = index;
-  //   });
-  //   _pageController.jumpToPage(index);
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: _addTaskButton(),
       body: _tasksList(),
       appBar: AppBar(title: const Text('Ana Sayfa')),
-      // body: PageView(
-      //   controller: _pageController,
-      //   onPageChanged: (index) {
-      //     setState(() {
-      //       _selectedIndex = index;
-      //     });
-      //   },
-      //   children: const [
-      //     PetListScreen(),
-      //     Center(child: Text('Evcil Hayvan Takip Uygulamasına Hoşgeldiniz!')),
-      //     SettingsScreen(),
-      //   ],
-      // ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: const <BottomNavigationBarItem>[
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.list),
-      //       label: 'Evcil Hayvanlar',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.home),
-      //       label: 'Ana Sayfa',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.settings),
-      //       label: 'Ayarlar',
-      //     ),
-      //   ],
-      //   currentIndex: _selectedIndex,
-      //   selectedItemColor: Colors.blue,
-      //   onTap: _onItemTapped,
-      // ),
     );
   }
 
